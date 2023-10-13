25c93f8ab0b72e4bf38999b3e52706d88756a43b
eLiz <ssyrmik@gmail.com>
2023-10-13 11:44:48 -0700

    svg rendering?

--- lib/spice_vault_web/controllers/web_controller.ex
+++ lib/spice_vault_web/controllers/web_controller.ex
@@ -3,7 +3,40 @@ defmodule SpiceVaultWeb.WebController do
   def index(conn, _params) do
     val = PolyGraph.add(1,2)
     graph = PolyGraph.render_svg()
-    render(conn, "graph.html", test: val, graph: graph)
+    testsvg =
+      """
+      <?xml version="1.0" encoding="UTF-8"?>
+      <svg xmlns="http://www.w3.org/2000/svg" width="96" height="214" viewBox="0 0 96 214">
+      <defs>
+        <marker id="startarrow" markerWidth="10" markerHeight="7" refX="0" refY="3.5" orient="auto">
+          <path d="M10 0v7L0 3.5z"/>
+        </marker>
+        <marker id="endarrow" markerWidth="10" markerHeight="7" refX="10" refY="3.5" orient="auto">
+          <path d="m0 0 10 3.5L0 7z"/>
+        </marker>
+      </defs>
+      <style>
+        .a14{font-size:14px;font-family:Times,serif}
+      </style>
+      <circle cx="57" cy="47" fill="#ffffffff" stroke="#000000ff" r="17"/>
+      <text text-anchor="middle" x="57" y="33" class="a14">
+        <tspan x="57" dy="1em">a</tspan>
+      </text>
+      <circle cx="57" cy="175" fill="#ffffffff" stroke="#000000ff" r="17"/>
+      <text text-anchor="middle" x="57" y="161" class="a14">
+        <tspan x="57" dy="1em">b</tspan>
+      </text>
+      <text text-anchor="middle" x="31" y="97" class="a14">
+        <tspan x="31" dy="1em">foo</tspan>
+      </text>
+      <path id="arrow0" d="M57 64v94" stroke="#000000ff" stroke-width="1" marker-end="url(#endarrow)" fill="transparent"/>
+      <text>
+        <textPath href="#arrow0" text-anchor="middle" class="a14"/>
+
+      </text>
+      </svg>
+      """
+    render(conn, "graph.html", test: val, graph: graph, testsvg: testsvg)
 
   end
 end
--- native/gen_graph/src/lib.rs
+++ native/gen_graph/src/lib.rs
@@ -1,35 +1,30 @@
-use graphviz_rust::dot_generator::*;
-use graphviz_rust::dot_structures::*;
-use graphviz_rust::{
-    attributes::*,
-    cmd::{CommandArg, Format},
-    exec, parse,
-    printer::{DotPrinter, PrinterContext},
-};
 
+
+use layout::gv;
+use layout::backends::svg::SVGWriter;
+use gv::GraphBuilder;
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
     a + b
 }
 #[rustler::nif]
 fn render_svg() -> String {
-    let  mut g = graph!(id!("id");
-         node!("nod"),
-         subgraph!("sb";
-             edge!(node_id!("a") => subgraph!(;
-                node!("n";
-                NodeAttributes::color(color_name::black), NodeAttributes::shape(shape::egg))
-            ))
-        ),
-        edge!(node_id!("a1") => node_id!(esc "a2"))
-    );
-    match(exec(
-         g,
-        &mut PrinterContext::default(),
-        vec![Format::Svg.into()],
-    )){
-        Ok(s)=>s,
-        Err(e)=>e.to_string(),
+    let contents = "digraph { a -> b [label=\"foo\"]; }";
+    let mut parser = gv::DotParser::new(&contents);
+    
+    match parser.process() {
+        Ok(g) => {
+            let mut svg = SVGWriter::new();
+            let mut gb = GraphBuilder::new();
+            gb.visit_graph(&g);
+            let mut vg = gb.get();
+            vg.do_it(false, false, false, &mut svg);
+            svg.finalize()
+
+        },
+        Err(err) => {
+            err
+        }
     }
 
 }
